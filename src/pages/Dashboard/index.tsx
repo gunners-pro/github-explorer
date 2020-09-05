bb0575a981a78b446834042857925d842c5d7400
Fabricyo Barreto <bass.barreto@gmail.com>
2020-09-04 21:04:13 -0300

    initial styling of the dashboard was completed.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Container, Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
-    <Container>
+    <>
+      <img src={logoImg} alt="Github Explorer Logo" />
       <Title>Explore repositórios no Github</Title>
-    </Container>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/65914504?s=460&u=4e7a3c23745091f3bcb29082ca2c61abf5d49be6&v=4"
+            alt="Fabricyo Barreto"
+          />
+          <div>
+            <strong>titulo do repositorio</strong>
+            <p>Aqui vai a descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   );
 };
 
